7bcde7d2b7dcdc4084a1717d45d089d5d592a6e1
Tyler Stephens <roberttstephens@gmail.com>
2014-02-17 04:00:34 +0000

    Getting base.html set up, creating styles.html which is a vomit of html/css to test

---
+++ app/static/js/functions.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	
+	/* Activate H5F */
+	H5F.setup(document.getElementsByTagName('form'));
+       
+   
+});
--- app/views.py
+++ app/views.py
@@ -5,3 +5,7 @@ from app import app
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/styles')
+def styles():
+    return render_template('styles.html')
