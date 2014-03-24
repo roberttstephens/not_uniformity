5e4e31926d8c96c1d976a25908763fb466874cb4
Tyler Stephens <roberttstephens@gmail.com>
2014-03-24 22:51:42 +0000

    Add the login manager for Flask-Login

--- app/__init__.py
+++ app/__init__.py
@@ -7,3 +7,15 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 
 from app import views, models
+
+# Login
+from flask.ext.login import LoginManager
+from app.models import Agency
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(id):
+    return Agency.query.get(int(id))
