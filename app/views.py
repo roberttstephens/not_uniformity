8ea67a1e468dbf0193efcb09283c8ba63918281e
Tyler Stephens <roberttstephens@gmail.com>
2014-03-24 22:51:24 +0000

    Add login and register views.

--- app/views.py
+++ app/views.py
@@ -1,8 +1,11 @@
-from flask import render_template
+from flask import Flask, session, request, flash, url_for, redirect, render_template, abort, g
+from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app
+from app.models import Agency
 
 @app.route('/')
 @app.route('/index', alias=True)
+@login_required
 def index():
     return render_template('index.html')
 
@@ -13,6 +16,35 @@ def caregiver_index():
 @app.route('/client')
 def client_index():
     return render_template('role_index.html', role='client')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    username = request.form['username']
+    password = request.form['password']
+    registered_user = Agency.query.filter_by(name=username, password=password).first()
+    if registered_user is None:
+        flash('Username or Password is invalid' , 'error')
+        return redirect(url_for('login'))
+    login_user(registered_user)
+    flash('Logged in successfully')
+    return redirect(request.args.get('next') or url_for('index'))
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register' , methods=['GET','POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    user = User(request.form['username'] , request.form['password'],request.form['email'])
+    db.session.add(user)
+    db.session.commit()
+    flash('User successfully registered')
+    return redirect(url_for('login'))
 
 @app.route('/styles')
 def styles():
@@ -29,3 +61,7 @@ def caregiver_form():
 @app.route('/client_form')
 def client_form():
     return render_template('role_form.html', role='client')
+
+@app.before_request
+def before_request():
+    g.user = current_user
