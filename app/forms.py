dabd28bce4ce8f2105da2c6b385c7f31436a4ff4
Tyler Stephens <roberttstephens@gmail.com>
2014-04-08 21:42:44 +0000

    Adding the registration form. Only a small part is done.

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,6 @@
-from wtforms.form import Form
+from flask.ext.wtf import Form
 from wtforms.fields import StringField, PasswordField
+from wtforms.fields.html5 import TelField, EmailField
 from wtforms import validators
 
 class LoginForm(Form):
@@ -11,3 +12,34 @@ class LoginForm(Form):
         validators.input_required(),
         validators.Length(min=8, max=1000)
     ])
+
+class RegisterForm(Form):
+    """
+    The registration form.
+    """
+    phone_number = TelField('Phone number', validators=[
+        validators.input_required(),
+    ])
+    phone_extension = TelField('Phone extension', validators=[
+        validators.Regexp('^\d*$', 0, 'Please only use numbers.'),
+        validators.Optional(),
+        validators.Length(min=1, max=10,
+            message='Please enter between 1 and 10 characters.')
+    ])
+    # TODO unique validator.
+    name = StringField('Agency name', validators=[
+        validators.input_required(),
+        validators.Length(min=3, max=128)
+    ])
+    #email VARCHAR(120), 
+    email = EmailField('Email', validators=[
+        validators.input_required(),
+        validators.Length(min=3, max=254),
+        validators.Email()
+    ])
+    #password VARCHAR(120), 
+    #access DATETIME, 
+    #contact_name VARCHAR(128) NOT NULL, 
+    #contact_title VARCHAR(128) NOT NULL, 
+    #status BOOLEAN NOT NULL, 
+    #address_id INTEGER NOT NULL, 
