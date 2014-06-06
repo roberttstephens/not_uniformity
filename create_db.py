fdff7b3ec4ed9e8bfd9d347e0c7a0063143fa360
Tyler Stephens <roberttstephens@gmail.com>
2014-06-06 06:16:17 +0000

    Remove guardian, add a supported living coach.

--- create_db.py
+++ create_db.py
@@ -23,11 +23,11 @@ abc.contact_title = 'Customer Service Representative'
 abc.phone_number = '9049389444'
 abc.phone_extension = '12345'
 abc.address = Address(
-    address_1 = '123 Fake st.',
-    address_2 = '',
-    city = 'Hello',
+    address_1 = '123 Fake St.',
+    address_2 = 'Apt 101',
+    city = 'Orlando',
     state = 'FL',
-    zip_code = '42239',
+    zip_code = '32817',
 )
 db.session.add(abc)
 db.session.commit()
@@ -90,28 +90,12 @@ first_aid_instance = CaregiverFormInstance(
 )
 db.session.add(first_aid_instance)
 db.session.commit()
-angela = Guardian(
-    name = 'Angela',
-    birth_date = date(1991, 1, 2),
-    phone_number = 4076781234,
-    phone_extension = '',
-    address = Address(
-        address_1 = '123 Hello',
-        address_2 = '',
-        city = 'Winter Park',
-        state = 'FL',
-        zip_code = '32817',
-    )
-)
-db.session.add(angela)
-db.session.commit()
 emmanuel = Client(
     name = 'Emmanuel',
     birth_date = date(1988, 1, 2),
     status = True,
     phone_number = 4076781234,
     phone_extension = '',
-    guardian = angela,
     agency = abc,
     address = Address(
         address_1 = '123 Hello',
@@ -122,5 +106,34 @@ emmanuel = Client(
     )
 )
 db.session.add(emmanuel)
+db.session.commit()
+supported_living_coach = Service(
+    name = 'supported living coach',
+    status = True,
+    caregiver = chase,
+    client = emmanuel,
+
+)
+db.session.add(supported_living_coach)
+db.session.commit()
+some_service_form = ServiceForm(
+    name = 'Some service form',
+    service = supported_living_coach
+)
+db.session.add(some_service_form)
+db.session.commit()
+some_service_form_instance = ServiceFormInstance(
+    service_form = some_service_form,
+    status = True,
+    expiration_date = date(2014, 1, 2),
+)
+db.session.add(some_service_form_instance)
+db.session.commit()
+some_other_service_form_instance = ServiceFormInstance(
+    service_form = some_service_form,
+    status = True,
+    expiration_date = date(2014, 6, 1),
+)
+db.session.add(some_other_service_form_instance)
 db.session.commit()
 exit()
