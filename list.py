00505d3ab3eb73fa8e32cb3a9c73e9d1f64b3fbb
Tyler Stephens <roberttstephens@gmail.com>
2014-06-06 06:15:20 +0000

    List services.

--- list.py
+++ list.py
@@ -2,6 +2,7 @@
 import os
 import readline
 from pprint import pprint
+from datetime import datetime
 
 from flask import *
 from app import *
@@ -17,9 +18,17 @@ for agency in agencies:
     print('Agency: ' + agency.name)
     for caregiver in agency.caregivers:
         print('Caregiver: ' + caregiver.name)
+        print('Expired ' + caregiver.expired)
+        print('Expiring soon ' + caregiver.expiring_soon)
+        for service in caregiver.services:
+            pprint(service.name)
+            pprint(service.client.name)
+            for service_form in service.forms:
+                for service_form_instance in service_form.instances:
+                    print('Instance of ' + service_form.name + 'expires on ' + service_form_instance.expiration_date.strftime("%Y-%m-%d"))
         for form in caregiver.forms:
             print('Caregiver form: ' + form.name)
-            for instance in form.instance:
+            for instance in form.instances:
                 print('Instance of ' + form.name + ' expires on ' + instance.expiration_date.strftime("%Y-%m-%d"))
                 if instance.received_date:
                     print('Instance of ' + form.name + ' received on ' + instance.received_date.strftime("%Y-%m-%d"))
