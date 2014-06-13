4499cf4a9dd531ebfe7bab05672bf060604738aa
Tyler Stephens <roberttstephens@gmail.com>
2014-06-13 02:42:41 -0400

    Hide the messages container if there are no more message.

--- app/static/js/functions.js
+++ app/static/js/functions.js
@@ -10,5 +10,10 @@ $(document).ready(function() {
   /* When the 'x' of a flashed message is clicked, close the message. */
   $('a.js-close-message').click(function () {
     $(this).parent().hide();
+    /* If there are no other visible messages, hide the message container. */
+    var visible_messages = $('section.js-message-container div.message').filter(':visible').length;
+    if (visible_messages === 0) {
+      $('section.js-message-container').hide();
+    }
   });
 });
