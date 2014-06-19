f157809fe2cbde045050cdb3e14e553ca8ad5946
Tyler Stephens <roberttstephens@gmail.com>
2014-06-19 01:38:42 -0400

    Redo the register form. More work todo.

--- app/forms.py
+++ app/forms.py
@@ -17,6 +17,32 @@ class RegisterForm(Form):
     """
     The registration form.
     """
+    # TODO unique validator.
+    name = StringField('Agency name', validators=[
+        validators.input_required(),
+        validators.Length(min=3, max=128)
+    ])
+    email = EmailField('Email', validators=[
+        validators.input_required(),
+        validators.Length(min=3, max=254),
+        validators.Email()
+    ])
+    password = PasswordField('Password', validators=[
+        validators.DataRequired(),
+        validators.Length(min=6, max=40)
+    ])
+    confirm = PasswordField('Verify password', validators=[
+        validators.DataRequired(),
+        validators.EqualTo('password', message='Passwords must match')
+    ])
+    contact_name = StringField('Contact name', validators=[
+        validators.input_required(),
+        validators.Length(min=3, max=128)
+    ])
+    contact_title = StringField('Contact title', validators=[
+        validators.input_required(),
+        validators.Length(min=3, max=128)
+    ])
     phone_number = TelField('Phone number', validators=[
         validators.input_required(),
     ])
@@ -29,23 +55,6 @@ class RegisterForm(Form):
             message='Please enter between 1 and 10 characters.'
         )
     ])
-    # TODO unique validator.
-    name = StringField('Agency name', validators=[
-        validators.input_required(),
-        validators.Length(min=3, max=128)
-    ])
-    #email VARCHAR(120),
-    email = EmailField('Email', validators=[
-        validators.input_required(),
-        validators.Length(min=3, max=254),
-        validators.Email()
-    ])
-    #password VARCHAR(120),
-    #access DATETIME,
-    #contact_name VARCHAR(128) NOT NULL,
-    #contact_title VARCHAR(128) NOT NULL,
-    #status BOOLEAN NOT NULL,
-    #address_id INTEGER NOT NULL,
 
 class EmailForm(Form):
     email = TextField(
