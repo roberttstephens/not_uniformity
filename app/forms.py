ceeed052668ff1770abe16c4e0d67b27d22598e2
Tyler Stephens <roberttstephens@gmail.com>
2014-06-22 00:30:49 -0400

    Create the caregiver form.

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,49 @@ from wtforms.fields import StringField, PasswordField, TextField, SelectField
 from wtforms.fields.html5 import TelField, EmailField
 from wtforms import validators
 
+class CaregiverForm(Form):
+    """
+    A form used to add/edit caregivers.
+    """
+    name = StringField('Name', validators=[
+        validators.input_required(),
+        validators.Length(min=2, max=128)
+    ])
+    email = EmailField('Email', validators=[
+        validators.input_required(),
+        validators.Length(min=3, max=254),
+        validators.Email()
+    ])
+    phone_number = TelField('Phone number', validators=[
+        validators.input_required(),
+    ])
+    phone_extension = TelField('Phone extension', validators=[
+        validators.Regexp('^\d*$', 0, 'Please only use numbers.'),
+        validators.Optional(),
+        validators.Length(
+            min=1,
+            max=10,
+            message='Please enter between 1 and 10 characters.'
+        )
+    ])
+    address_1 = StringField('Address 1', validators=[
+        validators.input_required(),
+        validators.Length(min=3, max=512)
+    ])
+    address_2 = StringField('Address 2', validators=[
+        validators.Optional(),
+        validators.Length(min=3, max=512)
+    ])
+    city = StringField('City', validators=[
+        validators.input_required(),
+        validators.Length(min=3, max=255)
+    ])
+    state = SelectField('State', choices=list(sorted(STATES.items())))
+    zip_code = StringField('Zip code', validators=[
+        validators.input_required(),
+        validators.Length(min=5, max=5)
+    ])
+
 class EmailForm(Form):
     email = TextField(
         'Email',
