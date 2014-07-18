3a48cff48ceac69a0f3dd007a9c649651785f628
Tyler Stephens <roberttstephens@gmail.com>
2014-07-18 00:08:04 -0400

    Load caregivers, clients, and services by default.

--- shell.py
+++ shell.py
@@ -5,5 +5,9 @@ from pprint import pprint
 
 from flask import *
 from app import *
+from app.models import *
+caregivers = db.session.query(Caregiver).all()
+clients = db.session.query(Client).all()
+services = db.session.query(Service).all()
 
 os.environ['PYTHONINSPECT'] = 'True'
